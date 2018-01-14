ab43cf45f0bc8fbfd3cc0c01b6a3b1f0506e2158
Brett Rogers <bdrogers@fullsail.edu>
2018-01-14 17:33:30 -0500

    Able to add items to cart

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 let expressHbs = require('express-handlebars');
 let mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const validator = require('express-validator');
+const MongoStore = require('connect-mongo')(session);
 
-// test merge
-
-var index = require('./routes/index');
-// var splash = require('./routes/splash');
+const index = require('./routes/index');
+const userRoutes = require('./routes/user');
 
 var app = express();
 
 mongoose.connect('localhost:27017/wd6international');
+require('./config/passport');
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
@@ -25,11 +29,27 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
+app.use(session({secret: 'mysupersecret', 
+                 resave: true, 
+                 saveUninitialized: false,
+                 store: new MongoStore({ mongooseConnection: mongoose.connection }),
+                 cookie: { maxAge: 180 * 60 * 1000 }
+                }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
+  next();
+})
+
+app.use('/user', userRoutes);
 app.use('/', index);
-// app.use('/splash', splash);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +13,30 @@ router.get('/', function(req, res, next) {
       productChunks.push(docs.slice(i, i+chunksSize));
     }
     res.render('shop/index', { title: 'WD6-International', products: productChunks });
-
   });
 });
+
+router.get('/add-to-cart/:id', (req, res, next) => {
+  let productId = req.params.id;
+  let cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, (err, product) => {
+    if (err) {
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
+  });
+});
+
+router.get('/shopping-cart', (req, res, next) => {
+  if(!req.session.cart) {
+    return res.render('shop/shopping-cart', {products:null});
+  }
+  let cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+})
+
 module.exports = router;
